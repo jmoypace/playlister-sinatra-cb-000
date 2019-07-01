aedb71397987ebce02a98e01558a836ddf07bc37
jmoypace <jmoypace@gmail.com>
2019-07-01 21:30:35 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,8 +7,8 @@ get '/artists' do
 erb:'artists/index'
 end
 
-get '/artists/:slug' do
-@artist = Artist.find_by_slug(params[:slug])
-erb :'artists/show'
-end
+#get '/artists/:slug' do
+#@artist = Artist.find_by_slug(params[:slug])
+#erb :'artists/show'
+#end
 end
